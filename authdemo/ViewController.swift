1ef5d306e6cc94bd8bb4a7c82643f7b3d22710bc
brainox <macmartins081@gmail.com>
2021-05-30 11:36:42 +0100

    implemented the firebase dummy login

--- authdemo/ViewController.swift
+++ authdemo/ViewController.swift
@@ -6,20 +6,52 @@
 //
 
 import UIKit
-import FirebaseAuth
+import FirebaseUI
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, FUIAuthDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Firebase Auth Demo"
     }
 
     @IBAction func loginTapped(_ sender: UIButton) {
         // Get the default auth ui object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            // Log the error
+            return
+        }
         // set ourselves as the delegate
-        // get a reference 
+        authUI?.delegate = self
+        authUI?.providers = [FUIEmailAuth()]
+        
+        // get a reference  to the auth ui view controller
+        let authViewController = authUI!.authViewController()
+        
+        // show it
+        present(authViewController, animated: true, completion: nil)
+        
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        // check if there was an error
+//        guard error == nil else {
+//            // log the error
+//            return
+//        }
+        // or
+        if error != nil {
+            // log error
+            print(error?.localizedDescription as Any)
+            return
+        }
+        performSegue(withIdentifier: "goHome", sender: self)
     }
     
 }
+
 
